cb0d5802b53e968c196870ee3de132cf605f76b2
maxxie114 <maxxie114@gmail.com>
2022-01-18 13:05:00 -0800

    create a merge conflict

--- filter-by-regex.py
+++ filter-by-regex.py
@@ -8,6 +8,7 @@
 
 # !!! You may need to run in your Shell: pip install pandas !!!
 # This line is written by Max AND SEBASTIAN
+# This will cause a merge conflict
 import os
 import pandas
 import re
